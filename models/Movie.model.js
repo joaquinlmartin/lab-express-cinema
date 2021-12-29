7f083f65276f24057298008d2c8aef00b95495ce
joaquinlmartin <joaquinlmartin@gmail.com>
2021-12-29 16:30:55 +0100

    done

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,13 +1,15 @@
-const {Schema, model} = require('mongoose');
-
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const movieSchema = new Schema({
-    title: String,
-    director: String,
-    stars: [String],
-    image: String,
-    description: String,
-    showtimes: [String],
+	title: { type: String, required: true },
+	director: { type: String, required: true },
+	stars: { type: [String], required: true },
+	image: { type: String, required: true },
+	description: { type: String, required: true },
+	showtimes: { type: [String], required: true },
 });
 
-module.exports = model('Movie', movieSchema);
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
